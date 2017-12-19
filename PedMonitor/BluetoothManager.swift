68955491a8978ffd97eaf238dd9161ef4f1672f4
Jay Tucker <jtucker@imprivata.com>
2017-12-19 10:55:10 -0500

    Added BTLE and pedometer functionality

---
+++ PedMonitor/BluetoothManager.swift
@@ -0,0 +1,116 @@
+//
+//  BluetoothManager.swift
+//  PedMonitor
+//
+//  Created by Jay Tucker on 12/19/17.
+//  Copyright © 2017 Imprivata. All rights reserved.
+//
+
+import Foundation
+import CoreBluetooth
+
+protocol BluetoothManagerDelegate {
+    func readMotionData(interval: TimeInterval, completion: @escaping (String) -> Void)
+}
+
+final class BluetoothManager: NSObject {
+    
+    private let serviceUUID                     = CBUUID(string: "B6108A9B-BF75-456B-8DCB-6942F2A3E5BA")
+    private let setIntervalCharacteristicUUID   = CBUUID(string: "AB519B66-6215-48D8-8727-4D41FB35DA8F")
+    private let getMotionDataCharacteristicUUID = CBUUID(string: "9D5F61B3-23E9-4812-8AF3-9536B9ADAC46")
+    
+    private var peripheralManager: CBPeripheralManager!
+    
+    var delegate:BluetoothManagerDelegate?
+    
+    var interval: TimeInterval = 10.0
+    
+    override init() {
+        super.init()
+        peripheralManager = CBPeripheralManager(delegate: self, queue: nil)
+    }
+    
+    private func addService() {
+        log("addService")
+        peripheralManager.stopAdvertising()
+        peripheralManager.removeAllServices()
+        let service = CBMutableService(type: serviceUUID, primary: true)
+        let setIntervalCharacteristic = CBMutableCharacteristic(type: setIntervalCharacteristicUUID, properties: .write, value: nil, permissions: .writeable)
+        let getMotionDataCharacteristic = CBMutableCharacteristic(type: getMotionDataCharacteristicUUID, properties: .read, value: nil, permissions: .readable)
+        service.characteristics = [setIntervalCharacteristic, getMotionDataCharacteristic]
+        peripheralManager.add(service)
+    }
+    
+    private func startAdvertising() {
+        log("startAdvertising")
+        peripheralManager.startAdvertising([CBAdvertisementDataServiceUUIDsKey: [serviceUUID]])
+    }
+    
+}
+
+extension BluetoothManager: CBPeripheralManagerDelegate {
+    
+    func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager) {
+        var caseString: String!
+        switch peripheral.state {
+        case .unknown:
+            caseString = "unknown"
+        case .resetting:
+            caseString = "resetting"
+        case .unsupported:
+            caseString = "unsupported"
+        case .unauthorized:
+            caseString = "unauthorized"
+        case .poweredOff:
+            caseString = "poweredOff"
+        case .poweredOn:
+            caseString = "poweredOn"
+        }
+        log("peripheralManagerDidUpdateState \(caseString!)")
+        if peripheral.state == .poweredOn {
+            addService()
+        }
+    }
+    
+    func peripheralManager(_ peripheral: CBPeripheralManager, didAdd service: CBService, error: Error?) {
+        let message = "peripheralManager didAddService " + (error == nil ? "ok" :  ("error " + error!.localizedDescription))
+        log(message)
+        if error == nil {
+            startAdvertising()
+        }
+    }
+    
+    func peripheralManagerDidStartAdvertising(_ peripheral: CBPeripheralManager, error: Error?) {
+        let message = "peripheralManagerDidStartAdvertising " + (error == nil ? "ok" :  ("error " + error!.localizedDescription))
+        log(message)
+    }
+    
+    func peripheralManager(_ peripheral: CBPeripheralManager, didReceiveWrite requests: [CBATTRequest]) {
+        log("didReceiveWriteRequests \(requests.count)")
+        for request in requests {
+            // let characteristic = request.characteristic
+            guard let value = request.value else {
+                log("request.value is nil")
+                return
+            }
+            log("received \(value.count) bytes:\(value.reduce("") { $0 + String(format: " %02x", $1) })")
+            guard let intervalString = String(data: value, encoding: .utf8), let interval = TimeInterval(intervalString) else {
+                log("couldn't parse interval")
+                return
+            }
+            log("setting interval to \(interval)")
+            self.interval = interval
+            peripheralManager.respond(to: request, withResult: .success)
+        }
+    }
+    
+    func peripheralManager(_ peripheral: CBPeripheralManager, didReceiveRead request: CBATTRequest) {
+        log("peripheralManager didReceiveRead request \(request.characteristic.uuid.uuidString)")
+        delegate?.readMotionData(interval: interval) { dataString in
+            log("data: \(dataString)")
+            request.value = dataString.data(using: .utf8, allowLossyConversion: false)
+            self.peripheralManager.respond(to: request, withResult: .success)
+        }
+    }
+    
+}
--- PedMonitor/ViewController.swift
+++ PedMonitor/ViewController.swift
@@ -7,10 +7,15 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var textView: UITextView!
+    
+    private var bluetoothManager: BluetoothManager!
+    
+    private let pedometer = CMPedometer()
     
     deinit {
         NotificationCenter.default.removeObserver(self)
@@ -21,18 +26,36 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         
         log("viewDidLoad")
-        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.appendMessage(_:)), name: NSNotification.Name(rawValue: newMessageNotificationName), object: nil)
+
+        NotificationCenter.default.addObserver(self, selector: #selector(didBecomeActive), name: .UIApplicationDidBecomeActive, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(willResignActive), name: .UIApplicationWillResignActive, object: nil)
+
+        NotificationCenter.default.addObserver(self, selector: #selector(appendMessage(_:)), name: NSNotification.Name(rawValue: newMessageNotificationName), object: nil)
+        
+        bluetoothManager = BluetoothManager()
+        bluetoothManager.delegate = self
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
         log("viewDidAppear")
+        
+        startPedometerUpdates()
+        startPedometerEventUpdates()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @objc func didBecomeActive() {
+        log(#function)
+    }
+    
+    @objc func willResignActive() {
+        log(#function)
     }
     
     @objc func appendMessage(_ notification: Notification) {
@@ -44,6 +67,64 @@ class ViewController: UIViewController {
             self.textView.scrollRangeToVisible(NSRange(location: newText.count, length: 0))
         }
     }
+    
+    private func startPedometerUpdates() {
+        guard CMPedometer.isDistanceAvailable() else { return }
+        
+        pedometer.startUpdates(from: Date()) { data, error in
+            guard error == nil else {
+                log("pedom error")
+                return
+            }
+            guard let data = data else {
+                log("pedom data is nil")
+                return
+            }
+            log("pedom \(data.numberOfSteps) \(String(format: "%.3f",data.distance!.doubleValue))")
+        }
+    }
+    
+    private func startPedometerEventUpdates() {
+        guard CMPedometer.isPedometerEventTrackingAvailable() else { return }
+        
+        pedometer.startEventUpdates { event, error in
+            guard error == nil else {
+                log("pedom event error")
+                return
+            }
+            guard let event = event else {
+                log("pedom event is nil")
+                return
+            }
+            let typeString: String
+            switch event.type {
+            case .pause: typeString = "pause"
+            case .resume: typeString = "resume"
+            }
+            log("pedom event \(typeString)")
+        }
+    }
 
 }
+
+extension ViewController: BluetoothManagerDelegate {
+    
+    func readMotionData(interval: TimeInterval, completion: @escaping (String) -> Void) {
+        // completion("1234")
+        let now = Date()
+        pedometer.queryPedometerData(from: now - interval, to: now) { data, error in
+            guard error == nil else {
+                log("error")
+                return
+            }
+            guard let data = data else {
+                log("data is nil")
+                return
+            }
+            completion("\(data.numberOfSteps)")
+        }
+    }
+    
+}
+
 
